edb318cd37ac6a351d753837a18eddfad211396a
Simon Mitchell <simon@3sidedcube.com>
2018-04-09 13:26:13 +0100

    Adds Preprocessor directives to make sure library works on all Apple OSes

---
+++ GeoJSONWatch/GeoJSONWatch.h
@@ -0,0 +1,19 @@
+//
+//  GeoJSONWatch.h
+//  GeoJSONWatch
+//
+//  Created by Simon Mitchell on 09/04/2018.
+//  Copyright © 2018 threesidedcube. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for GeoJSONWatch.
+FOUNDATION_EXPORT double GeoJSONWatchVersionNumber;
+
+//! Project version string for GeoJSONWatch.
+FOUNDATION_EXPORT const unsigned char GeoJSONWatchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GeoJSONWatch/PublicHeader.h>
+
+
